e43d4c3a11e166d4722548edb1a08daead59f251
Fernando Martins <fernandom.adm@gmail.com>
2025-07-29 20:08:35 -0300

    Adicionando configurações do Spring boot

--- anime-service/src/main/java/academy/devdojo/anime_service/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/anime_service/AnimeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ANimeServiceApplication {
+public class AnimeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ANimeServiceApplication.class, args);
+		SpringApplication.run(AnimeServiceApplication.class, args);
 	}
 
 }
